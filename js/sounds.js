a06a861854feaaaee4a5057f34283a637bf667a8
EmersomNunes <138039830+EmersomNunes@users.noreply.github.com>
2024-04-13 16:52:06 -0300

    looping sounds

--- js/sounds.js
+++ js/sounds.js
@@ -5,3 +5,9 @@ export const rain = new Audio("./assets/Chuva.wav");
 export const coffeShop = new Audio("./assets/Cafeteria.wav");
 
 export const fireplace = new Audio("./assets/Lareira.wav");
+
+
+florest.loop = true;
+rain.loop = true;
+coffeShop.loop = true;
+fireplace.loop = true;
